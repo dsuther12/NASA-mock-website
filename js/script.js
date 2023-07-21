332051ae3345ea88192d061d24f62e2ed932fcbf
Dylan Suther <dsuther@bgsu.edu>
2023-07-21 15:28:49 -0400

    Add more functionality and responsiveness

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function sideBar() {
+    var menuToggle = document.querySelector(".menu-toggle");
+    var hamburgerToggle = document.querySelector(".hamburger-menu")
+    hamburgerToggle.classList.toggle("active");
+    menuToggle.classList.toggle("active");
+    
+}
